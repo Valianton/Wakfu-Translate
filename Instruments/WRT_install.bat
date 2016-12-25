805526e1ed8801c921ed0f9057075d281d714e12
Anton <Valianton@mail.ru>
2016-12-25 12:31:48 +0500

    * исправлена ошибка русификации клиента

--- Instruments/WRT_install.bat
+++ Instruments/WRT_install.bat
@@ -1,4 +1,6 @@
 cd tmpTranslate
 Wakfu-Translate-master\Instruments\zip.exe -r ..\contents\gui_jar\gui.jar theme
-Wakfu-Translate-master\Instruments\zip.exe -r ..\contents\i18n\i18n_en.jar i18n\texts_en.properties
+cd i18n
+..\Wakfu-Translate-master\Instruments\zip.exe -r ..\..\contents\i18n\i18n_en.jar texts_en.properties
+cd ..
 cd ..
