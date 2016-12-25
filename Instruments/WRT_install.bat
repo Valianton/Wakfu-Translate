69be662f250c17f0daafd2e0f6bcbd3daad2b249
Anton <Valianton@mail.ru>
2016-12-25 12:23:53 +0500

    *исправлена ошибка

--- Instruments/WRT_install.bat
+++ Instruments/WRT_install.bat
@@ -1,4 +1,4 @@
 cd tmpTranslate
 Wakfu-Translate-master\Instruments\zip.exe -r ..\contents\gui_jar\gui.jar theme
-Wakfu-Translate-master\Instruments\zip.exe -r ..\contents\i18n\i18n_en.jar texts_en.properties
+Wakfu-Translate-master\Instruments\zip.exe -r ..\contents\i18n\i18n_en.jar i18n\texts_en.properties
 cd ..
